5446ef9abe00f69db16b6ee00af036d5beb04cf0
Rinshahan <rinshahan18@gmail.com>
2024-12-05 10:59:39 +0530

    added admin roles

--- src/app/Core/Service/user.service.ts
+++ src/app/Core/Service/user.service.ts
@@ -8,49 +8,50 @@ import { userlogin } from '../Models/userlogin';
   providedIn: 'root'
 })
 export class UserService {
-  showSearchBox: boolean =false
-  showCart:boolean=false
+  showSearchBox: boolean = false
+  showCart: boolean = false
   isLogged: boolean = false
-  
+
 
   logindatas: object[] = []
-  userlogin:userlogin[]=[]
-  signupUser:user[]=[]
+  userlogin: userlogin[] = []
+  signupUser: user[] = []
 
 
 
-  constructor(private toast:ToastrService,private router:Router) {
+  constructor(private toast: ToastrService, private router: Router) {
 
     const localdata = localStorage.getItem('user');
     if (localdata != null) {
       this.signupUser = JSON.parse(localdata);
     }
- 
-   }
 
-
-  signup(){
-    
-      localStorage.setItem('user',JSON.stringify(this.signupUser))
   }
 
 
-login(userId:string,Password:string){
-  this.logindatas.push(this.userlogin)
-  let findUser=this.signupUser.filter(user=>user.userid===userId && user.password===Password)
-if(findUser.length===0){
-this.toast.error('login credentials are invalid.')
-}else{this.router.navigate(['/home'])
-  this.toast.success('Login successful.')
-  this.isLogged = true
+  signup() {
 
-}
-}
-  
+    localStorage.setItem('user', JSON.stringify(this.signupUser))
+  }
 
-logout(){
-  localStorage.removeItem('user');
-}
-  
+
+  login(userId: string, Password: string) {
+    this.logindatas.push(this.userlogin)
+    let findUser = this.signupUser.filter(user => user.userid === userId && user.password === Password)
+    if (findUser.length === 0) {
+      this.toast.error('login credentials are invalid.')
+    } else {
+      this.router.navigate(['/home'])
+      this.toast.success('Login successful.')
+      this.isLogged = true
+
+    }
+  }
+
+
+  logout() {
+    localStorage.removeItem('user');
+  }
+
 
 }
